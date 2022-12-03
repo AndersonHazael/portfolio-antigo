6f32ba5617cf7c08ca06a917539376e7b00fc3da
AndersonHazael <117550667+AndersonHazael@users.noreply.github.com>
2022-12-03 00:10:32 -0300

    Add files via upload

---
+++ .js/exibe_text.js
@@ -0,0 +1,13 @@
+function exibeOne(){
+
+    titulo.textContent = "Apresentação"
+    texto.textContent = "Olá, meu nome é Anderson Ashael e eu sou um desenvolvedor front-end júnior. Minha apredizagem com desenvolvimento web ocorreu quando eu ainda era criança, foi quando tive meu primeiro contato com as linguagens de marcação HTML e CSS nas quais desenvolvi meus primeiros projetos, que até então, eram nada além de um hobbie! O desejo em atuar como profissional na área de programação só surgiu bem mais tarde, quando eu já estava no ensino médio.";
+
+}
+function exibeTwo(){
+    
+    titulo.textContent = "Experiências"
+    texto.textContent = "Foi no segundo ano do ensino médio que comecei a encarar a programação com mais seriedade! Trabalhei como menor aprendiz durante três meses em uma empresa local de João Pessoa/PB, utilizando desenvolvimento web para criar APPs voltados para Android e iOS. Me aprofundei em HTML, CSS e fui introduzido a novas tecnologias tais como Bootstrap, React Native, Ajax e Javascript.";
+    
+}
+
--- .js/navbar.js
+++ .js/navbar.js
@@ -1,1 +1,13 @@
+function exibeNavbar(){
 
+    if(itens.style.transform == 'translateX(100%)'){
+    header.style.height = '100vh';    
+    itens.style.transform = 'translateX(0)';
+    itens.style.transition = '0.3s';
+    }
+    else{
+        header.style.height = "10vh"
+        itens.style.transform = 'translateX(100%)'
+    }
+
+}
