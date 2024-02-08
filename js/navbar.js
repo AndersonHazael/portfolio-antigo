347e6603d67ca89531eef8eb9532c90753ac0a5f
AndersonHazael <117550667+AndersonHazael@users.noreply.github.com>
2024-02-08 13:16:48 -0300

    Update navbar.js
    Resolução do erro "Cannot read properties of null (reading 'style')" ocasionado devido ao JavaScript ser executado antes que o DOM seja devidamente carregado.

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,18 @@
+
+document.addEventListener('DOMContentLoaded', function(){ 
     let ulItens = document.getElementById('itens');
     let hHeader = document.getElementById('header');
 
-function exibeNavbar(){
-    if(ulItens.style.transform == 'translateX(100%)'){
-    hHeader.style.height = '100vh';    
-    ulItens.style.transform = 'translateX(0)';
-    ulItens.style.transition = '0.3s';
+    function exibeNavbar(){
+        if(ulItens.style.transform == 'translateX(100%)'){
+        hHeader.style.height = '100vh';    
+        ulItens.style.transform = 'translateX(0)';
+        ulItens.style.transition = '0.3s';
+        }
+        else{
+            hHeader.style.height = "10vh"
+            ulItens.style.transform = 'translateX(100%)'
+        }
+    
     }
-    else{
-        hHeader.style.height = "10vh"
-        ulItens.style.transform = 'translateX(100%)'
-    }
-
-}
+});
