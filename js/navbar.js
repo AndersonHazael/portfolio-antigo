f1cdb47895f8576e5cf3b24a614bcb8bfe96e462
AndersonHazael <117550667+AndersonHazael@users.noreply.github.com>
2024-02-08 13:06:27 -0300

    Update navbar.js
    itens = ulItens
    header = hHeader

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,14 @@
 function exibeNavbar(){
-
+    let ulItens = document.getElementbyId('itens');
+    let hHeader = document.getElementbyId('header');
     if(itens.style.transform == 'translateX(100%)'){
-    header.style.height = '100vh';    
-    itens.style.transform = 'translateX(0)';
-    itens.style.transition = '0.3s';
+    hHeader.style.height = '100vh';    
+    ulItens.style.transform = 'translateX(0)';
+    ulItens.style.transition = '0.3s';
     }
     else{
-        header.style.height = "10vh"
-        itens.style.transform = 'translateX(100%)'
+        hHeader.style.height = "10vh"
+        ulItens.style.transform = 'translateX(100%)'
     }
 
 }
